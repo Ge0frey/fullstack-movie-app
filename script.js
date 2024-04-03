4a4878a351e7a8353c7bd590371fd652fea0a24a
Ge0frey <jeffkimzyjoe@gmail.com>
2024-04-03 14:09:45 +0300

    changes in coloe pellet

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function returnMovies(url) {
     fetch(url)
     .then(res => res.json())
     .then(function(data){
-        console.log(data.results);
+        //console.log(data.results);
         data.results.forEach (element => {
             const div_card = document.createElement('div');
             div_card.setAttribute ("class", "card")
